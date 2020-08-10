212b36362441ec874a54ed35b282c047830bd9c7
Michał Doliński <micdolinski@gmail.com>
2020-08-10 20:04:09 +0200

    Init commit

---
+++ game.py
@@ -0,0 +1,58 @@
+import pygame as pg
+
+
+class Game():
+    def __init__(self, name='Game', w=512, h=512):
+        self._isRunning = True
+        self._screen = None
+        self._objects = []
+        self._width = w
+        self._height = h
+        self._init()
+        pg.display.set_caption(name)
+        
+
+    # initialization, screen etc.
+    def _init(self):
+        pg.init()
+        size = (self._width, self._height)
+        self.screen = pg.display.set_mode(size)
+        pg.display.set_icon(pg.image.load('images/game_icon.png'))
+
+    # game logic
+    def _handleLoop(self):
+        loopHandlers = [o for o in self._objects if callable(getattr(o, 'onLoop', None))]
+        for o in loopHandlers:
+            loopHandlers.onLoop()
+
+
+    # pygame event handling
+    def _handleEvents(self):
+        eventHandlers = [o for o in self._objects if callable(getattr(o, 'onEvent', None))]
+        for event in pg.event.get():
+            # quit
+            if event.type == pg.QUIT:
+                self._isRunning = False
+            # others...
+            for o in eventHandlers:
+                o.onEvent(event)
+
+
+    # pygame drawing
+    def _handleDraw(self):
+        drawHandlers = [o for o in self._objects if callable(getattr(o, 'onEvent', None))]
+        for o in drawHandlers:
+            o.draw(self._screen)
+        pg.display.flip()
+
+    # user requested exit
+    def _cleanup(self):
+        pg.quit()
+
+    # starts game and keeps it running
+    def run(self):
+        while self._isRunning:
+            self._handleLoop()
+            self._handleEvents()
+            self._handleDraw()
+        self._cleanup()
